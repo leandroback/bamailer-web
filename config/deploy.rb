b74ac6cf7432da11097ba89014763be6da618521
Martin Aceto <maceto@backing-online.com>
2013-08-16 18:25:34 -0300

    change deploy to mongoid.yml link automated

--- config/deploy.rb
+++ config/deploy.rb
@@ -34,15 +34,15 @@ namespace :deploy do
   task :setup_config, :roles => :app do
     sudo "ln -nfs #{current_path}/config/nginx.conf /usr/local/nginx/sites-enabled/#{application}"
     run "mkdir -p #{shared_path}/config"
-    #put File.read("config/database.example.yml"), "#{shared_path}/config/database.yml"
+    put File.read("config/mongoid_example.yml"), "#{shared_path}/config/mongoid.yml"
     puts "Now edit the config files in #{shared_path}."
   end
   after "deploy:setup", "deploy:setup_config"
 
-  #task :symlink_config, :roles => :app do
-  #  run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
-  #end
-  #after "deploy:finalize_update", "deploy:symlink_config"
+  task :symlink_config, :roles => :app do
+    run "ln -nfs #{shared_path}/config/mongoid.yml #{release_path}/config/mongoid.yml"
+  end
+  after "deploy:finalize_update", "deploy:symlink_config"
 
 end
 
